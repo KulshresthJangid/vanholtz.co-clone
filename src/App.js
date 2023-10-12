e0734d5346199e8c9ee872d4c0e4e7e41f6492cb
KulshresthJangid <kulshresthjangid@gmail.com>
2023-10-13 01:31:44 +0530

    First ever commit

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <ul className="projects">
+      <li className="projectsLi">
+        <div className="projectContainer">
+          <a href="#">Here click it.....</a>
+          </div>
+      </li>
+      <li className="projectsLi">
+        <div className="projectContainer">
+          <a href="#">Here click it.....</a>
+          </div>
+      </li>
+      <li className="projectsLi">
+        <div className="projectContainer">
+          <a href="#">Here click it.....</a>
+          </div>
+      </li>
+      <li className="projectsLi">
+        <div className="projectContainer">
+          <a href="#">Here click it.....</a>
+          </div>
+      </li>
+      <li className="projectsLi">
+        <div className="projectContainer">
+          <a href="#">Here click it.....</a>
+          </div>
+      </li><li className="projectsLi">
+        <div className="projectContainer">
+          <a href="#">Here click it.....</a>
+          </div>
+      </li>
+      <li className="projectsLi">
+        <div className="projectContainer">
+          <a href="#">Here click it.....</a>
+          </div>
+      </li>
+    </ul>
+  );
+}
+
+export default App;
