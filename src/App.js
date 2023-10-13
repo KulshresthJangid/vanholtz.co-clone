c2a4ad05286ee9d8c041105c33a8bb91e933fa94
KulshresthJangid <kulshresthjangid@gmail.com>
2023-10-14 01:35:12 +0530

    Fixed the prespective alignmenty

--- src/App.js
+++ src/App.js
@@ -3,42 +3,51 @@ import './App.css';
 
 function App() {
   return (
-    <ul className="projects">
-      <li className="projectsLi">
-        <div className="projectContainer">
-          <a href="#">Here click it.....</a>
-          </div>
-      </li>
-      <li className="projectsLi">
-        <div className="projectContainer">
-          <a href="#">Here click it.....</a>
-          </div>
-      </li>
-      <li className="projectsLi">
-        <div className="projectContainer">
-          <a href="#">Here click it.....</a>
-          </div>
-      </li>
-      <li className="projectsLi">
-        <div className="projectContainer">
-          <a href="#">Here click it.....</a>
-          </div>
-      </li>
-      <li className="projectsLi">
-        <div className="projectContainer">
-          <a href="#">Here click it.....</a>
-          </div>
-      </li><li className="projectsLi">
-        <div className="projectContainer">
-          <a href="#">Here click it.....</a>
-          </div>
-      </li>
-      <li className="projectsLi">
-        <div className="projectContainer">
-          <a href="#">Here click it.....</a>
-          </div>
-      </li>
-    </ul>
+    <div className="stage">
+      <div className="stageContainer">
+        <ul className="projects">
+          <li className="projectsLi">
+            <div className="projectContainer">
+              <a href="#">Studio Mega</a>
+            </div>
+          </li>
+          <li className="projectsLi">
+            <div className="projectContainer">
+              <a href="#">The brigade</a>
+            </div>
+          </li>
+          <li className="projectsLi">
+            <div className="projectContainer">
+              <a href="#">PUSH</a>
+            </div>
+          </li>
+          <li className="projectsLi">
+            <div className="projectContainer">
+              <a href="#">ONIX MAPS</a>
+            </div>
+          </li>
+          <li className="projectsLi">
+            <div className="projectContainer">
+              <a href="#">ICON MOTO-SPORTS</a>
+            </div>
+          </li><li className="projectsLi">
+            <div className="projectContainer">
+              <a href="#">Parliament</a>
+            </div>
+          </li>
+          <li className="projectsLi">
+            <div className="projectContainer">
+              <a href="#">HAYDEN-SHAPES</a>
+            </div>
+          </li>
+          <li className="projectsLi">
+            <div className="projectContainer">
+              <a href="#">ZERVICE</a>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
   );
 }
 
