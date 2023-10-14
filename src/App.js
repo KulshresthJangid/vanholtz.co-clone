9a847b1fa4ac265b1404f3ae136f4a362dcd8a66
KulshresthJangid <kulshresthjangid@gmail.com>
2023-10-14 15:46:52 +0530

    fixed hover

--- src/App.js
+++ src/App.js
@@ -1,53 +1,9 @@
 import logo from './logo.svg';
-import './App.css';
+import Projects from './Projects';
 
 function App() {
   return (
-    <div className="stage">
-      <div className="stageContainer">
-        <ul className="projects">
-          <li className="projectsLi">
-            <div className="projectContainer">
-              <a href="#">Studio Mega</a>
-            </div>
-          </li>
-          <li className="projectsLi">
-            <div className="projectContainer">
-              <a href="#">The brigade</a>
-            </div>
-          </li>
-          <li className="projectsLi">
-            <div className="projectContainer">
-              <a href="#">PUSH</a>
-            </div>
-          </li>
-          <li className="projectsLi">
-            <div className="projectContainer">
-              <a href="#">ONIX MAPS</a>
-            </div>
-          </li>
-          <li className="projectsLi">
-            <div className="projectContainer">
-              <a href="#">ICON MOTO-SPORTS</a>
-            </div>
-          </li><li className="projectsLi">
-            <div className="projectContainer">
-              <a href="#">Parliament</a>
-            </div>
-          </li>
-          <li className="projectsLi">
-            <div className="projectContainer">
-              <a href="#">HAYDEN-SHAPES</a>
-            </div>
-          </li>
-          <li className="projectsLi">
-            <div className="projectContainer">
-              <a href="#">ZERVICE</a>
-            </div>
-          </li>
-        </ul>
-      </div>
-    </div>
+    <Projects />
   );
 }
 
