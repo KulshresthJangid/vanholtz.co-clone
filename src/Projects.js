a46fdabc517839d220e774762f7ec8c1e37a4559
KulshresthJangid <kulshresthjangid@gmail.com>
2023-10-27 02:12:23 +0530

    header complete

--- src/Projects.js
+++ src/Projects.js
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Projects.css';
 
 export default function Projects() {
+
+  const [activeItem, setActiveItem] = useState(null);
+
+  const activeTheLink = () => {
+    console.log("clicked")
+  }
+
   return (
     <div className="projects-main">
     <div className="stagePerspective parallaxPerspective perspective-origin-tt-bb">
       <div className="stageContainer" style={{ opacity: 1, height: '2327px' }}>
         <main className="stage" style={{ transform: 'translate3d(0px, 0px, 0px)' }}>
           <ul className="projects isHovering">
-            <li className="projectsLi" data-barba="HomeTransition">
+            <li className="projectsLi" data-barba="HomeTransition" onClick={() => activeTheLink()}>
               <div className="projectContainer">
                 <a href="/work/studiomega/"  data-barba="HomeTransition">Studio<br />Mega</a>
               </div>
